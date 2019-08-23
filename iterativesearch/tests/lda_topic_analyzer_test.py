2a979efca1aebc0a9c5b3e6043a1bbc2948c8c26
Vale Tolpegin <valetolpegin@gmail.com>
2019-08-22 21:16:36 -0400

    Finalize initial version of LDA topic analyzer

--- iterativesearch/tests/lda_topic_analyzer_test.py
+++ iterativesearch/tests/lda_topic_analyzer_test.py
@@ -9,10 +9,36 @@ import nltk
 import unittest
 from ..lda_builder import LDABuilder
 from loguru import logger
+import os
 
 class LDATopicAnalyzerTestCase(unittest.TestCase):
 
-    # TODO : Add tests to verify non-null parameters
+    @classmethod
+    def setUpClass(cls):
+        cls.newsgroups_train = fetch_20newsgroups(subset="train", shuffle = False)
+        cls.newsgroups_test = fetch_20newsgroups(subset="test", shuffle = False)
+
+        nltk.download('wordnet')
+
+        cls.processed_docs = []
+        for doc in cls.newsgroups_train.data:
+            cls.processed_docs.append(cls.preprocess(doc))
+
+        cls.dictionary = gensim.corpora.Dictionary(cls.processed_docs)
+        cls.dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
+        cls.bow_corpus = [cls.dictionary.doc2bow(doc) for doc in cls.processed_docs]
+
+        cls.builder = LDABuilder(logger)
+        cls.builder.set_num_topics(10).set_passes(5).set_workers(4).set_random_state(1).set_dictionary(cls.dictionary).set_corpus(cls.bow_corpus)
+
+        cls.lda_topic_analyzer = cls.builder.build()
+        cls.lda_topic_analyzer.train_model()
+
+    @classmethod
+    def tearDownClass(cls):
+        for file in os.listdir("."):
+            if "tmp" in file:
+                os.remove(file)
 
     def test_builder_construction_pass(self):
         num_topics = 10
@@ -33,58 +59,34 @@ class LDATopicAnalyzerTestCase(unittest.TestCase):
         self.assertEqual(lda_topic_analyzer.corpus, corpus)
         self.assertEqual(lda_topic_analyzer.random_state, random_state)
 
-    def lemmatize_stemming(self, text):
+    @classmethod
+    def lemmatize_stemming(cls, text):
         return SnowballStemmer("english").stem(WordNetLemmatizer().lemmatize(text, pos='v'))
 
-    # Tokenize and lemmatize
-    def preprocess(self, text):
+    @classmethod
+    def preprocess(cls, text):
         result=[]
         for token in gensim.utils.simple_preprocess(text):
             if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
-                result.append(self.lemmatize_stemming(token))
+                result.append(cls.lemmatize_stemming(token))
 
         return result
 
     def test_lda_topic_analyzer_function(self):
-        newsgroups_train = fetch_20newsgroups(subset="train", shuffle = False)
-        newsgroups_test = fetch_20newsgroups(subset="test", shuffle = False)
-
-        nltk.download('wordnet')
-
-        processed_docs = []
-        for doc in newsgroups_train.data:
-            processed_docs.append(self.preprocess(doc))
-
-        dictionary = gensim.corpora.Dictionary(processed_docs)
-        dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
-        bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
-
-        builder = LDABuilder(logger)
-        builder.set_num_topics(10).set_passes(5).set_workers(4).set_random_state(1).set_dictionary(dictionary).set_corpus(bow_corpus)
-
-        lda_topic_analyzer = builder.build()
-        lda_topic_analyzer.generate_lda_model()
-
-        self.assertIsNotNone(lda_topic_analyzer.get_topics())
+        self.assertIsNotNone(self.lda_topic_analyzer.get_topics())
 
     def test_lda_topic_analyzer_prediction(self):
-        newsgroups_train = fetch_20newsgroups(subset="train", shuffle = False)
-        newsgroups_test = fetch_20newsgroups(subset="test", shuffle = False)
+        self.assertIsNotNone(self.lda_topic_analyzer.predict_topics(self.bow_corpus[1]))
 
-        nltk.download('wordnet')
+    def test_lda_model_save_pass(self):
+        self.assertFalse(os.path.exists("tmp.lda"))
+        self.lda_topic_analyzer.save_model("tmp.lda")
+        self.assertTrue(os.path.exists("tmp.lda"))
 
-        processed_docs = []
-        for doc in newsgroups_train.data:
-            processed_docs.append(self.preprocess(doc))
+        new_lda_topic_analyzer = self.builder.build()
 
-        dictionary = gensim.corpora.Dictionary(processed_docs)
-        dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
-        bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+        self.assertFalse(new_lda_topic_analyzer.is_model_trained)
+        new_lda_topic_analyzer.load_model("tmp.lda")
+        self.assertTrue(new_lda_topic_analyzer.is_model_trained)
 
-        builder = LDABuilder(logger)
-        builder.set_num_topics(10).set_passes(5).set_workers(4).set_random_state(1).set_dictionary(dictionary).set_corpus(bow_corpus)
-
-        lda_topic_analyzer = builder.build()
-        lda_topic_analyzer.generate_lda_model()
-
-        self.assertIsNotNone(lda_topic_analyzer.predict_topics(bow_corpus[1]))
+        self.assertIsNotNone(new_lda_topic_analyzer.predict_topics(self.bow_corpus[1]))
